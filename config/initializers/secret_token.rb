084c52acacc4691c42c71635c2b4b7b0e3c93e2e
Marco Berardini <mberardini@evolution-man.com>
2013-11-28 13:26:22 -0800

    Created Social app and status model

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Social::Application.config.secret_token = '9f4c71a6007d789c69b9688a84fd9b827905ebb2a2b1d6b5c6841fab71330685efe3463a0aa6f5ac4fdd5365ba641e14460b244958f7b55c17600b87f7982c07'
